9bea1e12b933137be8f65a44eebd66b46e1870e2
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-04-03 20:22:19 -0300

    Att

--- frontend/src/pages/Entertainment/index.js
+++ frontend/src/pages/Entertainment/index.js
@@ -5,6 +5,7 @@ import { Container, Ul } from './styles'
 export default function Entertainment() {
   return (
     <Container>
+      <div>Adicionar Recomendação de Entretenimento</div>
       <Ul>
         <li>
           <strong>Título</strong>
@@ -38,12 +39,15 @@ export default function Entertainment() {
 
         <li>
           <strong>Título</strong>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
-            perferendis doloribus voluptas hic, ratione recusandae dicta cum
-            exercitationem aliquam molestias sequi mollitia dignissimos
-            voluptate, ex amet fugiat est voluptates quibusdam.
-          </p>
+          <iframe
+            title="vvar3"
+            width="500"
+            height="200"
+            src="https://www.youtube.com/embed/BS0s1jxzL1g"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
           <span>por Franco, 15 horas atrás</span>
           <button type="button">Ver noticia completa</button>
         </li>
